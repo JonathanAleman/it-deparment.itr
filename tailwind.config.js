cae3593addaa499f2fb8f8cf022c615468ec71e8
Jonathan Alemán <jonathan.maleman20@gmail.com>
2021-02-09 01:14:55 -0600

    Hecha sección de proyectos y preguntas frecuentes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,16 @@ module.exports = {
       },
       colors:{
         primary:{
-          purple:'#4132E4'
+          black:'#141313',
+          blue:'#213EEA',
         },
         secondary:{
-          gray:'#777777',
+          'gray-10':'#777777',
+          'gray-9': '#999999',
+          'gray-6': 'rgba(255,255,255,0.6)',
+          'black-10': 'rgba(119,119,119,0.1)',
+          'purple-1':'#4132E4',
+          'purple-2': '#786DF0',
         }
       },
       backgroundImage:{
