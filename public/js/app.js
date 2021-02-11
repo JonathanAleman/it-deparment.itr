e2a89a7c0194089dfef80a212ea28371e0fa5088
Jonathan Alemán <jonathan.maleman20@gmail.com>
2021-02-11 13:58:55 -0600

    Se agrego otro carousel y se modificaron los SlideToShow

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,37 @@
 
 window.addEventListener('load', function(){
+  new Glider(document.querySelector('.container__list3'), {
+      // Mobile-first defaults
+      slidesToShow: 1,
+      dots: '.dots',
+      arrows: {
+        prev: '.carousel-prev',
+        next: '.carousel-next'
+      },
+      responsive: [
+        {
+          // screens greater than >= 775px
+          breakpoint: 550,
+          settings: {
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: 1   ,
+            slidesToScroll: 'auto',
+            duration: 0.75
+          }
+        },{
+          // screens greater than >= 1024px
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            itemWidth: 150,
+            duration: 0.75
+          }
+        }
+      ]
+    });
+});
+window.addEventListener('load', function(){
     new Glider(document.querySelector('.container__list'), {
         // Mobile-first defaults
         slidesToShow: 1,
@@ -47,7 +79,7 @@ window.addEventListener('load', function(){
           breakpoint: 550,
           settings: {
             // Set to `auto` and provide item width to adjust to viewport
-            slidesToShow: 2   ,
+            slidesToShow: 1   ,
             slidesToScroll: 'auto',
             itemWidth: 150,
             duration: 0.75
@@ -56,7 +88,7 @@ window.addEventListener('load', function(){
           // screens greater than >= 1024px
           breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
+            slidesToShow: 1,
             slidesToScroll: 1,
             itemWidth: 150,
             duration: 0.75
